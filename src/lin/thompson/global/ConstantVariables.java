f4e3821de70594baaf1d50021695620b805e42c9
unclemm <hsiang.lin23@gmail.com>
2014-03-19 21:20:30 -0400

    DeckFactory, ConstantVariables
    Made a hard coded factory. Refractor some stuff into a new class called
    ConstantVariables

---
+++ src/lin/thompson/global/ConstantVariables.java
@@ -0,0 +1,12 @@
+package lin.thompson.global;
+
+import java.util.HashMap;
+
+import android.graphics.Picture;
+
+public class ConstantVariables {
+	  public static String[] faces = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+	  public static String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+	  public static HashMap<String,Picture> map = new HashMap<String,Picture>();
+	  
+}
