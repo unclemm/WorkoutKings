fd9150be2809901f2dd20f622525154e0bd6ab1d
unclemm <hsiang.lin23@gmail.com>
2014-03-30 00:33:08 -0400

    1

--- src/lin/thompson/factory/HardCodeFactory.java
+++ src/lin/thompson/factory/HardCodeFactory.java
@@ -3,19 +3,24 @@ package lin.thompson.factory;
 import lin.thompson.deck.CardDeck;
 
 public class HardCodeFactory implements DeckFactory{
-
+	private CardDeck deck;
 	@Override
 	public CardDeck initializeCardDeck() {
-		// TODO Auto-generated method stub
-		return null;
+		deck = new CardDeck();
+		return deck;
 	}
-
+	
+	public ____ getCardPicture(String suit, String number) {
+		
+	}
+	
 	@Override
 	public String getExerciseName(String suit) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
 	@Override
 	public String getNumOfReps(String cardNumber) {
 		// TODO Auto-generated method stub
